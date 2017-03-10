62442102be5d611c9f45914fc2bca2bf8186901d
“newayliu1” <newayliu1@gmail.com>
2017-03-10 11:29:13 -0500

    Implement Clickable Marker
    Worked with Dan and Cathy
    - Added infowindow in index.html files which will be called when a user
    search a specific space.
    - Modified google map functionalities in order to let user search a
    single restaurant information.
    
    Next will be working on CSS STYLE to make the browser more functional.

--- assets/mapinit.js
+++ assets/mapinit.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.initAutocomplete = function () {
+window.initAutocomplete = function() {
   let defaultPosition = {
     lat: 42.364506,
     lng: -71.038887
@@ -14,65 +14,49 @@ window.initAutocomplete = function () {
   //   map: map
   // });
 
-  // creates the search box and links it to UI
   let input = document.getElementById('pac-input');
-  let searchBox = new google.maps.places.SearchBox(input);
+
+  let autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
+  let infowindow = new google.maps.InfoWindow();
+  let infowindowContent = document.getElementById('infowindow-content');
+  infowindow.setContent(infowindowContent);
+  let marker = new google.maps.Marker({
+    map: map
   });
-  let markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    let places = searchBox.getPlaces();
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
 
-    if (places.length == 0) {
+  autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    let place = autocomplete.getPlace();
+    if (!place.geometry) {
       return;
     }
 
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+
+    // Set the position of the marker using the place ID and location.
+    marker.setPlace({
+      placeId: place.place_id,
+      location: place.geometry.location
     });
-    markers = [];
+    marker.setVisible(true);
 
-    // For each place, get the icon, name and location.
-    let bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      let icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
+    infowindowContent.children['place-name'].textContent = place.name;
+    infowindowContent.children['place-id'].textContent = place.place_id;
+    infowindowContent.children['place-address'].textContent =
+      place.formatted_address;
+    infowindow.open(map, marker);
   });
-}
 
-// module.exports = {
-//   initAutocomplete,
-// };
+};
