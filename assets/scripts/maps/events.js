ce750b7a1dc55da7fab7bc20eda25cafe6e9a00d
dsweetser <daniel.sweetser@gmail.com>
2017-03-10 19:22:51 -0500

    Added Feature to get address from selected pin
    Worked with Wei and Cathy
    Implemented feature that will let us take the address from a clicked on
    pin an import it into the new restaurant field.
    Next step is to merge this into master and start a new branch for trying
    to get multiple pins to work.

--- assets/scripts/maps/events.js
+++ assets/scripts/maps/events.js
@@ -1,20 +1,20 @@
 'use strict';
 
-// const googleMapsClient = require('@google/maps').createClient({
-//   key: 'AIzaSyDZ0_kQ56P2uibaCsMRXADbk1e25BgloPA'
-// });
-//
-//
-// const addHandlers = function () {
-//   $(window).load(function() {
-//     map = new google.maps.Map(document.getElementById('map'), {
-//       center: {lat: 42.364506, lng: -71.038887},
-//       zoom: 8
-//     });
-//   });
-//
-// };
-//
-// module.exports = {
-//   addHandlers,
-// };
+const getDataFromMaps = function(event) {
+  event.preventDefault();
+  let place = {};
+  place.name = $('#place-name').text();
+  place.address = $('#place-address').text();
+  // $('#restaurantName').val('');
+  // $('#restaurantAddress').val('');
+  $('#restaurantName').val(place.name);
+  $('#restaurantAddress').val(place.address);
+};
+
+const addHandlers = function () {
+  $('.getDataFromMaps').on('click', getDataFromMaps);
+};
+
+module.exports = {
+  addHandlers,
+};
